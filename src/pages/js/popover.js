2f6e96f82885022eab68ffa6bed4117f801cdfe8
Nehal Patel <nehalvpatels+github@gmail.com>
2016-12-07 00:31:40 -0600

    Improved formatting for discussion score, age, and comments

--- src/pages/js/popover.js
+++ src/pages/js/popover.js
@@ -1,6 +1,22 @@
 require("../css/popover.css");
 var Vue = require("vue/dist/vue.js");
 
+const time_formats = [
+    [60, "seconds", 1], // 60
+    [120, "1 minute ago", "1 minute from now"], // 60*2
+    [3600, "minutes", 60], // 60*60, 60
+    [7200, "1 hour ago", "1 hour from now"], // 60*60*2
+    [86400, "hours", 3600], // 60*60*24, 60*60
+    [172800, "1 day ago", "1 day from now"], // 60*60*24*2
+    [604800, "days", 86400], // 60*60*24*7, 60*60*24
+    [1209600, "1 week ago", "1 week from now"], // 60*60*24*7*4*2
+    [2419200, "weeks", 604800], // 60*60*24*7*4, 60*60*24*7
+    [4838400, "1 month ago", "1 month from now"], // 60*60*24*7*4*2
+    [29030400, "months", 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+    [58060800, "1 year ago", "1 year from now"], // 60*60*24*7*4*12*2
+    [2903040000, "years", 29030400] // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+];
+
 var popover;
 document.addEventListener("DOMContentLoaded", function(event) {
     popover = new Vue({
@@ -31,11 +47,6 @@ window.addEventListener("message", function (msg) {
 }, false);
 
 function renderDiscussions(discussions, tab) {
-    let now = new Date();
-    let date_now = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds()); 
-    let date_entry; 
-    let one_day = 86400000; // milliseconds per days
-    
     let orderIn = safari.extension.settings.orderIn;
     let orderUsing = safari.extension.settings.orderUsing;
 
@@ -57,13 +68,13 @@ function renderDiscussions(discussions, tab) {
     let permalinks = [];
     for (let i = 0; i < keysSorted.length; i++) {
         let entry = discussions[keysSorted[i]].data;
-        date_entry = new Date(entry.created_utc * 1000).getTime();
+
         permalinks[i] = {
             permalink: "https://www.reddit.com" + entry.permalink,
             title: entry.title,
-            score: entry.score,
-            age: ((date_now - date_entry) / one_day).toFixed(1) + " days ago",
-            comments: entry.num_comments,
+            score: formatScore(entry.score),
+            age: formatAge(new Date(entry.created_utc * 1000)),
+            comments: entry.num_comments.toLocaleString(),
             subreddit: entry.subreddit,
         };
     }
@@ -73,4 +84,45 @@ function renderDiscussions(discussions, tab) {
     popover.links = permalinks;
 
     window.scrollTo(0, 0);
+}
+
+function formatScore(num) {
+    return num > 999 ? (num/1000).toFixed(1) + "k" : num;
+}
+
+function formatAge(time) {
+    switch (typeof time) {
+        case "number": break;
+        case "string": time = +new Date(time); break;
+        case "object": if (time.constructor === Date) time = time.getTime(); break;
+        default: time = +new Date();
+    }
+
+    let seconds = (+new Date() - time) / 1000;
+    let token = "ago";
+    let list_choice = 1;
+
+    if (seconds == 0) {
+        return "Just now";
+    }
+
+    if (seconds < 0) {
+        seconds = Math.abs(seconds);
+        token = "from now";
+        list_choice = 2;
+    }
+
+    let i = 0
+    let format;
+    while (format = time_formats[i++]) {
+        if (seconds < format[0]) {
+            if (typeof format[2] == "string") {
+                return format[list_choice];
+            } else {
+                return Math.floor(seconds / format[2]) + " " + format[1] + " " + token;
+            }
+        }
+    }
+
+    return time;
 }
